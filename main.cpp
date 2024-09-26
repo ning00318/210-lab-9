400f6f5fbf526c72d97507d891dd7144558a5a63
NINA <ninatsao@ningningning.lan>
2024-09-25 19:04:14 -0700

    210-lab-9

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     // complete this code in order to make it output
     // the numbers 1-100
-
-    for ()
+    const int SIZE = 100;
+    for (int i = 1; i < SIZE; i++)
         cout << i << " ";
 
     return 0;
